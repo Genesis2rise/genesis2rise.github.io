e6620f538d977025dd35405ffa629b0f6551aa95
Tulsi Bhagat <Tulsibhagat50@gmail.com>
2022-08-25 13:31:39 +0545

    Updated all pages
    Updated all pages

--- wiki/load.php
+++
@@ -1,3 +0,0 @@
-/* This file is the Web entry point for MediaWiki's ResourceLoader:
-   <https://www.mediawiki.org/wiki/ResourceLoader>. In this request,
-   no modules were requested. Max made me put this here. */
